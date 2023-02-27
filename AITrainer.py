c536aea251a1692ad474809b2cc476ae2f6fbe2a
colinsheppard10 <colinsheppard10@gmail.com>
2023-02-26 19:14:18 -0500

    bar and percent counter

--- AITrainer.py
+++ AITrainer.py
@@ -1,5 +1,8 @@
 import cv2
 import mediapipe as mp
+import numpy as np
+
+from ExerciseCounter import findAngle
 
 
 # current tutorial: https://youtu.be/4WwSJAKRtcA - using landmarks
@@ -11,7 +14,8 @@ pose = mpPose.Pose()
 cap = cv2.VideoCapture(0)
 
 up = False
-counter = 0
+count = .5
+dir = 0
 
 while True: 
     ret, img = cap.read()
@@ -30,24 +34,47 @@ while True:
 
         cv2.circle(img, points[12], 15, (255,0,0), cv2.FILLED)
         cv2.circle(img, points[14], 15, (255,0,0), cv2.FILLED)
-        cv2.circle(img, points[11], 15, (255,0,0), cv2.FILLED)
-        cv2.circle(img, points[13], 15, (255,0,0), cv2.FILLED)
+        cv2.circle(img, points[16], 15, (255,0,0), cv2.FILLED)
 
-        if not up and points[14][1] + 40 < points[12][1]:
-            print("UP")
-            up = True
-            counter += 1
-        elif points[14][1] > points[12][1]:
-            print("Down")
-            up = False
+        angle = findAngle(points, 12, 14, 16)
 
-    cv2.putText(img, str(counter), (100,150),cv2.FONT_HERSHEY_PLAIN, 12, (255,0,0),12)
+        # # Left Arm
+        #angle = detector.findAngle(img, 11, 13, 15,False)
+        per = np.interp(angle, (210, 310), (0, 100))
+        bar = np.interp(angle, (220, 310), (650, 100))
+        # print(angle, per)
+
+        # Check for the dumbbell curls
+        print('per:',per, ' dir:', dir, ' count:', count)
+        color = (255, 0, 255)
+        if per == 100:
+            color = (0, 255, 0)
+            if dir == 0:
+                count += 0.5
+                dir = 1
+        if per == 0:
+            color = (0, 255, 0)
+            if dir == 1:
+                count += 0.5
+                dir = 0
+
+        # Draw Bar
+        cv2.rectangle(img, (1100, 100), (1175, 650), color, 3)
+        cv2.rectangle(img, (1100, int(bar)), (1175, 650), color, cv2.FILLED)
+        cv2.putText(img, f'{int(per)} %', (1100, 75), cv2.FONT_HERSHEY_PLAIN, 4,
+                    color, 4)
+
+        # Draw Curl Count
+        cv2.rectangle(img, (0, 450), (250, 720), (0, 255, 0), cv2.FILLED)
+        cv2.putText(img, str(int(count)), (45, 670), cv2.FONT_HERSHEY_PLAIN, 15,
+                    (255, 0, 0), 25)
+        
     cv2.imshow('frame', img)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
 # After the loop release the cap object
-vid.release()
+cap.release()
 # Destroy all the windows
 cv2.destroyAllWindows()
--- Exercise.py
+++ Exercise.py
@@ -1,17 +1,4 @@
-import math
-
-def findAngle(points, p1, p2, p3):
-
-    # Get the landmarks
-    x1, y1 = points[p1]
-    x2, y2 = points[p2]
-    x3, y3 = points[p3]
-
-    # Calculate the Angle
-    angle = math.degrees(math.atan2(y3 - y2, x3 - x2) -
-                          math.atan2(y1 - y2, x1 - x2))
-    if angle < 0:
-        angle += 360
-
-    # print(angle)
-    return angle
+class Exercise: 
+    def __init__(self, points, startAngle, endAngle, bar, percent, count):
+      
+        
